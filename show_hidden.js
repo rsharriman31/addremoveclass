b6d4b32a9d54a48d37a07a64d2c49a35c23065b7
rsharriman31 <75581743+rsharriman31@users.noreply.github.com>
2021-01-01 10:04:50 -0500

    Update and rename activity.js to show_hidden.js

---
+++ show_hidden.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+
+  $("#q1").on('click', function() {
+    // WRITE YOUR CODE HERE
+  });
+
+  $("#q2").on('click', function() {
+    // WRITE YOUR CODE HERE
+  });
+
+  $("#q3").on('click', function() {
+    // WRITE YOUR CODE HERE
+  });
+
+  $("#q4").on('click', function() {
+    // WRITE YOUR CODE HERE
+  });
+
+  $("#q5").on('click', function() {
+    // WRITE YOUR CODE HERE
+  });
+
+  $("#q6").on('click', function() {
+    // WRITE YOUR CODE HERE    
+  });
+
+});
